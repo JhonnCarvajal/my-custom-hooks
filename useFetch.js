aae6ae08a5315329864203331993acf8d1428e05
JhonnyCarvajal <JhonnCarvajal@outlook.com>
2021-11-29 11:39:30 -0400

    First commit

---
+++ useFetch.js
@@ -0,0 +1,42 @@
+import { useEffect, useRef, useState } from "react";
+
+export const useFetch = (url) => {
+  const isMounted = useRef(true);
+  useEffect(() => {
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+  const [state, setState] = useState({
+    data: null,
+    loading: true,
+    error: null,
+  });
+  useEffect(() => {
+    setState({
+      data: null,
+      loading: true,
+      error: null,
+    });
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        if (isMounted.current) {
+          setState({
+            data: data,
+            loading: false,
+            error: null,
+          });
+        } else {
+          console.log("Se desmonto el componente");
+        }
+      }).catch(()=>{
+        setState({
+          data:null,
+          loading:false,
+          error:'No se pudo cargar la info'
+        })
+      })
+  }, [url]);
+  return state;
+};
